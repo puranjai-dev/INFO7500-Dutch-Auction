4c265e770c3aaacbdb693edb9b6b45dfdfef1df6
Puranjai Mendiratta <mendiratta.p@northeastern.edu>
2023-06-07 00:21:20 -0400

    Added v2 for DutchAuction project

---
+++ v2/hardhat.config.ts
@@ -0,0 +1,10 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.18",
+};
+
+export default config;
+
+require('@openzeppelin/hardhat-upgrades');
